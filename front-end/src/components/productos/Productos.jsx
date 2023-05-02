3ac76f87674ee8d58b2824df556fd732e45dbc74
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-05-02 16:56:08 -0700

    fix producto

--- front-end/src/components/productos/Productos.jsx
+++ front-end/src/components/productos/Productos.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import style from './productos.module.css';
+import React from "react";
+import style from "./productos.module.css";
 
 export const Productos = () => {
   return (
-    <div className="product-grid">
-      <div className="product-item">
+    <div className={style.productGrid}>
+      <div className={style.productItem}>
         <img src="product-1.jpg" alt="Product 1" />
-          <h3>Product 1</h3>
-          <p>$19.99</p>
+        <h3>Product 1</h3>
+        <p>$19.99</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Productos;
