a735e692cbcac18d20f004da3e2a2a0d19ab41d1
Ian Salas López <96995669+Salasian@users.noreply.github.com>
2023-05-04 18:52:29 -0700

    Inicio sesión terminado

--- front-end/src/components/inicio/Inicio.jsx
+++ front-end/src/components/inicio/Inicio.jsx
@@ -1,43 +1,55 @@
-import React from 'react'
+import React from "react";
 import style from "./inicio.module.css";
 
 export const Inicio = () => {
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className={`col-sm-6 ${style.columna}`}>
-          <h1>Bienvenidos al mejor salon de belleza con los mejores servicios</h1>
-          <p>Ana Cecilia Salon es un salon de belleza que preocupa todos sus servicios hacias sus clientes seaon de la mejor calidad y la mejor experiencia, entre los servicios se ofrecen:</p>
-          <ul  className={`${style.muestras}`}>
+    <div className="container">
+      <div className="row">
+        <article className={`col-sm-6 ${style.columna}`}>
+          <h1>
+            Bienvenidos al mejor salon de belleza con los mejores servicios
+          </h1>
+          <p>
+            Ana Cecilia Salon es un salón de belleza que procura servicios de
+            alta calidad hacia sus clientes sean de la mejor calidad y la mejor
+            experiencia, entre los servicios se ofrecen:
+          </p>
+          <div className={`d-flex ${style.muestras} `}>
             <li>
-              <img style={{width: '100%'}} src="/cortes.jpg" />
-              <p>Cortes y peinados</p>
+              <img src="/cortes.jpg" alt="servicio" />
+              <p className="text-center">Cortes y peinados</p>
             </li>
             <li>
-            <img style={{width: '120%', height: '215px'}} src="/maquillaje.jpg" />
-              <p>Maquillajes</p>
+              <img src="/maquillaje.jpg" alt="servicio" />
+              <p className="text-center">Maquillajes</p>
             </li>
             <li>
-            <img style={{width: '210%'}} src="/tratamiento.jpg" />
-              <p>Tratamientos capilares y más</p>
+              <img src="/tratamiento.jpg" alt="servicio" />
+              <p className="text-center">Tratamientos capilares y más</p>
             </li>
-          </ul>
-        </div>
-        <div className='col-sm-6'>
-          <div className={`${style.sesion}`}>
-            <button>Registrate</button>
-            <div style={{width: '100%', height: '5px'}}>
-              <hr style={{border: 'none', borderTop: '1px solid #999'}} />
-            </div> 
-            <button>Iniciar Sesion</button>
           </div>
-          <div className={`${style.descubre}`}>
-          <p className='text-center'><strong>Descubre mas servicios</strong></p>
+        </article>
+        <div className="col-1"></div>
+        <aside className={`col-4 d-flex flex-column ${style.menuContainer}`}>
+          <div className={`d-flex flex-column ${style.sesion}`}>
+            <h4>Registrate</h4>
+            <p className="text-center">
+              Si no tienes una cuenta registrada y deseas agendar servicios y
+              ver los productos de la estética
+            </p>
+            <button>Crea cuenta</button>
+            <section className={`d-flex ${style.separatorBox}`}>
+              <div className={`col-5 flex-fill ${style.separator}`}></div>
+              <div className={`col-1 text-center ${style.sesionText}`}>ó</div>
+              <div className={`col-5 flex-fill ${style.separator}`}></div>
+            </section>
+            <h5>¿Ya tienes una cuenta?</h5>
+            <button>Acceder</button>
           </div>
-        </div>
+        </aside>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Inicio;
--- front-end/src/components/productos/Productos.jsx
+++ front-end/src/components/productos/Productos.jsx
@@ -3,29 +3,58 @@ import style from "./productos.module.css";
 
 export const Productos = () => {
   return (
-    <div className={style.productGrid}>
-      <div className={style.productItem}>
-        <img src="product-1.jpg" alt="imagen" />
+    <div className="d-flex container justify-content-center">
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto1.jpg"
+          alt="imagen"
+        />
         <h3>Titulo</h3>
         <p>Descripción</p>
       </div>
-      <div className={style.productItem}>
-        <img src="product-1.jpg" alt="imagen" />
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto2.jpg"
+          alt="imagen"
+        />
         <h3>Titulo</h3>
         <p>Descripción</p>
       </div>
-      <div className={style.productItem}>
-        <img src="product-1.jpg" alt="imagen" />
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto3.jpg"
+          alt="imagen"
+        />
         <h3>Titulo</h3>
         <p>Descripción</p>
       </div>
-      <div className={style.productItem}>
-        <img src="product-1.jpg" alt="imagen" />
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto4.jpg"
+          alt="imagen"
+        />
         <h3>Titulo</h3>
         <p>Descripción</p>
       </div>
-      <div className={style.productItem}>
-        <img src="product-1.jpg" alt="imagen" />
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto5.jpg"
+          alt="imagen"
+        />
+        <h3>Titulo</h3>
+        <p>Descripción</p>
+      </div>
+      <div className={`col-2 ${style.productItem}`}>
+        <img
+          className={style.productPicture}
+          src="../../producto5.jpg"
+          alt="imagen"
+        />
         <h3>Titulo</h3>
         <p>Descripción</p>
       </div>
